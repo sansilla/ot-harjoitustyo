5dfe72ce8a3f3a45ecf7566f310f4f81c453fe78
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-22 10:42:40 +0200

    testejä  lisää

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,23 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 35.00 euroa")
+
+    def test_riittaako_rahat(self):
+        self.maksukortti.ota_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_rahat_ei_riita(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+
