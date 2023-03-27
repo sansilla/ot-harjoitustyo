5c5c8e607cc8bfe3a0f6d9cbb400e9c3429c94ef
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-27 19:42:06 +0300

    säätöö

--- src/base/user.py
+++ src/base/user.py
@@ -2,4 +2,3 @@ class User:
     def __init__(self, name, password):
         self.name = name
         self.password = password
-
---
+++ src/creating/create_user.py
@@ -0,0 +1,11 @@
+from base.user import User
+
+class AboutUsers:
+    def __init__(self):
+        #jotain konstruktoriin
+
+    def show_users(self):
+        #näyttää listan käyttäjistä
+
+    def create_user(self, user):
+        #luo uuden käyttäjän
