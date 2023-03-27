7f127dd38eaa2fc44a4d6d705dd136ba87cfb8e2
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-27 19:05:47 +0300

    diaryn konstruktori määritelty

---
+++ src/base/diary.py
@@ -0,0 +1,7 @@
+import time
+
+class Diary:
+    def __init__(self, note, date, user=None):
+        self.note = note
+        self.date = date #sovellus asettaisi tämän automaattisesti uuteen kirjaukseen
+        self.user = user
