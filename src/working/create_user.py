567075d582fe8485b655bdf9138b878c1a9604e5
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-27 19:45:13 +0300

    muoks

--- src/working/create_user.py
+++ src/working/create_user.py
@@ -1,8 +1,8 @@
 from base.user import User
 
 class AboutUsers:
-    def __init__(self, operation):
-        self.operation = operation
+    def __init__(self):
+        #konstruktoriin jotain
 
     def show_users(self):
         #näyttää listan käyttäjistä
