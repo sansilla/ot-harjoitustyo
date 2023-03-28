33ac4e55adaf9a8d586497efb0b0b8af701cea1f
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-28 16:21:56 +0300

    muoks2

--- src/working/create_user.py
+++ src/working/create_user.py
@@ -1,11 +1,14 @@
 from base.user import User
+import sql_database
 
 class AboutUsers:
     def __init__(self):
         #konstruktoriin jotain
 
-    def show_users(self):
+    #def show_users(self):
         #näyttää listan käyttäjistä
+        # ! ! SIIRRETTY DATABASE-OSIOON ! !
 
-    def create_user(self, user):
+    #def create_user(self, user):
         #luo uuden käyttäjän
+        # ! ! SIIRRETTY DATABASE -OSIOON ! !
