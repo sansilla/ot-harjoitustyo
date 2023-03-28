80684d880f67187616d743ff0838607252172183
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-28 16:21:02 +0300

    muoks

---
+++ src/working/create_note.py
@@ -0,0 +1,13 @@
+from base.diary import Diary
+
+class Note:
+    def __init__(self):
+        #jotain asiaa tänne
+
+    #def new_note(self):
+        #kirjaa uusi merkintä
+        # ! ! SIIRRETTY DATABASE -OSIOON ! !
+
+    #def show_all(self):
+        #näyttää kaikki merkinnät
+        # ! ! SIIRRETTY DATABASE -OSIOON ! !
