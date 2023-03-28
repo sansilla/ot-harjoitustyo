170f361aeb6095b99effff3d2241db437ca6728d
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-28 15:07:17 +0300

    toimi

--- src/sql_database.py
+++ src/sql_database.py
@@ -1,1 +1,9 @@
+import os
 import sqlite3
+
+db = sqlite3.connect("courses.db")
+db.isolation_level = None
+
+def create_tables():
+    db.execute("CREATE TABLE Users_table (id INTEGER PRIMARY KEY, name TEXT)")
+    db.execute("CREATE TABLE Notes_table (id INTEGER PRIMATY KEY, user_id INTEGER REFERENCES Users_table, note TEXT, date DATETIME)")
