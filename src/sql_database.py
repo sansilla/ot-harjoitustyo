3825547da56f11ce8ac67ca93816a8bd385e1922
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-28 15:50:53 +0300

    käskyjä

--- src/sql_database.py
+++ src/sql_database.py
@@ -8,13 +8,20 @@ def create_tables():
     db.execute("CREATE TABLE Notes_table (id INTEGER PRIMATY KEY, user_id INTEGER REFERENCES Users_table, note TEXT, date DATETIME)")
 
 def show_users():
-    users = db.execute("SELECT name FROM Users_table").fetchall()
+    users = db.execute("SELECT name FROM Users_table ORDER BY name").fetchall()
     for user in users:
         print(user)
 
-def create_user():
+def create_user(name):
     name = input("Käyttäjänimi: ")
     db.execute("INSERT INTO Users_table (name) VALUES (?)", [name])
     print("Uusi käyttäjä luotu!")
 
+def show_all(name):
+    notes = db.execute(f"SELECT note FROM Notes_table, Users_table WHERE Notes_table.user_id = Users_table.id AND Users_table.name = '{name}'").fetchall()
+    for note in notes:
+        print(note)
 
+def new_note(note):
+    note = input("Päivän havainnot: ")
+    db.execute("")
