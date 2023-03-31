d7c412e26e98c66326a8c94942256e29b13565b6
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-31 14:04:18 +0300

    turhat kommentoinnit pois

--- src/sql_database.py
+++ src/sql_database.py
@@ -12,20 +12,6 @@ def get_some_service():
 #db = sqlite3.connect("weatherdiary.db") #Pitäisikö tietokantayhteys muodostaa täysin omassa moduulissaan?
 #db.isolation_level = None
 
-#def create_tables():
-    #db.execute("CREATE TABLE Users_table (id INTEGER PRIMARY KEY, name TEXT)")
-    #db.execute("CREATE TABLE Notes_table (id INTEGER PRIMATY KEY, user_id INTEGER REFERENCES Users_table, date DATETIME, note TEXT)")
-
-#def show_users():
-    #users = db.execute("SELECT name FROM Users_table ORDER BY name").fetchall()
-    #for user in users:
-        #print(user)
-
-#def create_user(name):
-    #name = input("Luo käyttäjänimi: ")
-    #db.execute("INSERT INTO Users_table (name) VALUES (?)", [name])
-    #print("Uusi käyttäjä luotu!")
-
 #def show_all(name):
     #notes = db.execute(f"SELECT note FROM Notes_table, Users_table WHERE Notes_table.user_id = Users_table.id AND Users_table.name = '{name}'").fetchall()
     #for note in notes:
