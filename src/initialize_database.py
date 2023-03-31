cb9af795a6f2b28c28b0d2921c0b4cefee678da2
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-31 10:01:04 +0300

    muutoksia

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -1,7 +1,14 @@
-from sql_database import ???
+from sql_database import ???1
 
-def create_tables(???):
+def create_tables(connection):
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE Users_table (id INTEGER PRIMARY KEY, name TEXT)")
     cursor.execute("CREATE TABLE Notes_table (id INTEGER PRIMATY KEY, user_id INTEGER REFERENCES Users_table, date DATETIME, note TEXT)")
     connection.commit()
+
+def initialize_sql_database():
+    connection = ???1
+    create_tables(connection)
+
+if __name__ == "__main__":
+    initialize_sql_database()
