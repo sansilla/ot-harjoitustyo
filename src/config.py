f2434978bad2887df7ea58d25739a67d29548897
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-31 12:33:04 +0300

    muutoksii

--- src/config.py
+++ src/config.py
@@ -5,4 +5,7 @@ d_name = os.path.d_name(__file__)
 
 try:
     load_dotenv(dotenv_path=os.path.join(d_name, "..", ".env"))
-except File
+except FileNotFoundError:
+    pass
+
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
