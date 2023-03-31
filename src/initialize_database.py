147b73a546694bf750f36f6a83c02784ea044662
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-03-31 10:24:43 +0300

    yhteys databaseen

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -1,14 +1,14 @@
-from sql_database import ???1
+from sql_database import get_some_service
 
-def create_tables(connection):
-    cursor = connection.cursor()
+def create_tables(service):
+    cursor = service.cursor()
     cursor.execute("CREATE TABLE Users_table (id INTEGER PRIMARY KEY, name TEXT)")
     cursor.execute("CREATE TABLE Notes_table (id INTEGER PRIMATY KEY, user_id INTEGER REFERENCES Users_table, date DATETIME, note TEXT)")
-    connection.commit()
+    service.commit()
 
 def initialize_sql_database():
-    connection = ???1
-    create_tables(connection)
+    service = get_some_service()
+    create_tables(service)
 
 if __name__ == "__main__":
     initialize_sql_database()
