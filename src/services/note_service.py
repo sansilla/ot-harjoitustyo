d14660c1317315cc46eae80828187ce346fb54cf
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-04-17 13:59:35 +0300

    lisäyksiä

---
+++ src/services/note_service.py
@@ -0,0 +1,21 @@
+from base.diary import Diary
+from base.user import User
+
+from working.create_note import (create_note as default_note_re)
+# jatka create_note tiedostoa, siellä ei ole vielä kaikkea tarvittavaa
+
+from working.create_user import (create_user as default_user_re)
+
+class InvalidCredentialsError(Exception):
+    pass
+
+class UsernameAlreadyExistsError(Exception):
+    pass
+
+class NoteService:
+    def __init__(self, create_note=default_note_re, create_user=default_user_re):
+        self._user = None
+        self._create_note = create_note
+        self._create_user = create_user
+
+    def diary_note(self, )
