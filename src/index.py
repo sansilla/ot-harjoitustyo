17b130b55d841e8d9ff53acd071fb4bf24102c6d
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-04-17 09:02:02 +0300

    index päivitys

---
+++ src/index.py
@@ -0,0 +1,14 @@
+from tkinter import Tk
+from ui.ui import UI
+
+def main():
+    window = Tk()
+    window.title("Sääpäiväkirja")
+
+    ui_view = UI(window)
+    ui_view.start()
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
