6c125c1ac4ab2363a12e1a0e0e9c6efb56666f82
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-04-17 09:45:12 +0300

    lookkia päivitetty

--- src/ui/create_user_look.py
+++ src/ui/create_user_look.py
@@ -1,1 +1,40 @@
 from tkinter import ttk, StringVar, constants
+from services #importtaa asioita: UsernameAlreadyExistsError
+
+class CreateUserLook:
+    def __init__(self, root, handle_creating, handle_showing_login):
+        self._root = root
+        self._handle_creating = handle_creating
+        self._handle_showing_login = handle_showing_login
+        self._frame = None
+        self._username_entry = None
+        self._error_variable = None
+        self._error_label = None
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _create_user_handler(self):
+        username = self._username_entry.get()
+
+        if len(username) == 0:
+            self._show_error("Syötä käyttäjänimi")
+            return
+        
+        try:
+            # services -kansiosta asiaa
+            self._handle_creating()
+        except UsernameAlreadyExistsError: #tämä vielä luomatta
+            self._show_error(f"Käyttäjänimi {username} on jo olemassa")
+
+    def _show_error(self, message):
+        self._error_variable.set(message)
+        self._error_label.grid()
+
+    def _hide_error(self):
+        self._error_label.grid_remove()
