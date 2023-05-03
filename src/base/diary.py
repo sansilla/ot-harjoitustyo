087ba815cc41ba0cd12f3b4b545ca2b7c79c5618
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2023-05-03 13:30:39 +0300

    docstring aloitettu

--- src/base/diary.py
+++ src/base/diary.py
@@ -1,8 +1,16 @@
-import time
+#import time
 
 
 class Diary:
+    """Luokka, joka kuvaa yhtä muistiinpanoa
+    """
     def __init__(self, note, user=None):
+        """Konstruktori, joka muodostaa muistiinpanon
+
+        Args:
+            note: merkkijono, joka kertoo säähavainnon
+            user (optional): oletukseltaan None
+        """
         self.note = note
         #self.date = date  # sovellus asettaisi tämän automaattisesti uuteen kirjaukseen
         self.user = user
--- src/base/user.py
+++ src/base/user.py
@@ -1,4 +1,11 @@
 class User:
+    """Luokka käyttäjälle
+    """
     def __init__(self, name):
+        """Konstruktori, joka tekee käyttäjän
+
+        Args:
+            name: merkkijono, joka on käyttäjän käyttäjänimi
+        """
         self.name = name
         # self.password = password
